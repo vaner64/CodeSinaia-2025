fae43d895576d8b8ded85d1337427888d03066b1
Vlad <vlad.ciurea@saguna.ro>
2025-07-21 16:30:11 +0300

    shaorma done

--- IntroToPy/Randoms_1.py
+++ IntroToPy/Randoms_1.py
@@ -2,8 +2,8 @@ import random
 
 # Read input parameters from the console
 count = 100 # int(input("Number of values?> "))
-min_value = 15 # int(input("Minimum value?> "))
-max_value = 65 # int(input("Maximum value?> "))
+min_value = 10 # int(input("Minimum value?> "))
+max_value = 200 # int(input("Maximum value?> "))
 print(f"Generating {count} randoms in the range [{min_value}, {max_value}]")
 
 # Generate count values in the range [min_value, max_value] and store them in a the values map
@@ -18,7 +18,7 @@ for i in range(0, count):
     randoms_map[r].append(i)
 
 # Write a text file "randoms_db.txt" with each random on a line, its value followed by the indexes where it occurred
-with open("IntroToPy/randoms_db.txt", "w") as data_file:
+with open("IntroToPy/shaorma_db.txt", "w") as data_file:
     for r in randoms_map.keys():
         data_file.write(f"{r} {randoms_map[r]}\n")
 
