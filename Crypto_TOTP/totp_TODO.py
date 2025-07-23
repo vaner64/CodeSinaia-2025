c86d62670d9fbcd6ba966fefdc2af8ed34375a16
Vlad <vlad.ciurea@saguna.ro>
2025-07-23 22:23:02 +0300

    x

--- Crypto_TOTP/totp_TODO.py
+++ Crypto_TOTP/totp_TODO.py
@@ -24,12 +24,14 @@ def gen_qr(user_id):
 
     secret = base64.b32encode(generate_shared_secret()).decode('utf-8')     # generate secret key
     
-    # TODO: combine code# and user id to create URI (hint: match example URI format given above)
-    uri = None
+    # : combine code# and user id to create URI (hint: match example URI format given above)
+    uri = code1 + user_id + code2 + secret + code3
     print(" >> URI generated: ", uri)
 
-    # TODO: store secret into a file named "secret.txt"
-    file = None
+    # : store secret into a file named "secret.txt"
+    file = open("secret.txt", "w")
+    file.write(secret)
+    file.close()
 
     # TODO: generate QR code based on the URI using snego library
     qrcode = segno.make(uri, micro=False)
@@ -43,9 +45,9 @@ def generate_otp(secret_base32, digits=6, time_step=30):
     # decode the base32-encoded secret string into raw bytes
     key = base64.b32decode(secret_base32, casefold=True)
 
-    # TODO: get the current time step
-    current_time = None
-    counter = None
+    # : get the current time step
+    current_time = int(time.time())
+    counter = current_time // time_step
 
     # convert the counter to an 8-byte big-endian byte array
     counter_bytes = struct.pack(">Q", counter)
@@ -57,17 +59,17 @@ def generate_otp(secret_base32, digits=6, time_step=30):
     # this chooses where to start slicing the hash
     offset = hmac_hash[-1] & 0x0F       # value between 0 and 15
 
-    # TODO: take 4 bytes of hmac hash starting at the offset
-    selected_bytes = None
+    # : take 4 bytes of hmac hash starting at the offset
+    selected_bytes = hmac_hash[offset:offset + 4]
 
     # convert those 4 bytes to a big-endian integer
     code_int = struct.unpack(">I", selected_bytes)[0]
 
-    # TODO: remove the sign bit; Hint: 0x7FFFFFFF = 0111 1111 1111 1111 1111 1111 1111 1111
-    code_int = None
+    # : remove the sign bit; Hint: 0x7FFFFFFF = 0111 1111 1111 1111 1111 1111 1111 1111
+    code_int = code_int & 0x7FFFFFFF
 
     # TODO: get only 6 digits; Hint: mod 10^?
-    otp = None
+    otp = code_int % (10 ** digits)
 
     return otp
 
@@ -75,8 +77,8 @@ def generate_otp(secret_base32, digits=6, time_step=30):
 # ========= function for displaying OPT every 30 sec ========
 def get_otp(t=30):
     # TODO: open and read file containing secret; Hint: use readline
-    file = None
-    secret = None
+    file = open("secret.txt", "r")
+    secret = file.readline()
     file.close()
 
     while True:
